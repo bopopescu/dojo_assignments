d07ed9928c70082fafb5220d8808994b5dfdd854
Victor De Vera <victordevera24@gmail.com>
2018-03-14 17:13:42 -0700

    updating commit

---
+++ Python/mySQL/friendships_friendships.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: friendships
+-- ------------------------------------------------------
+-- Server version	5.7.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `friendships`
+--
+
+DROP TABLE IF EXISTS `friendships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friendships` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) DEFAULT NULL,
+  `friend_id` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friendships`
+--
+
+LOCK TABLES `friendships` WRITE;
+/*!40000 ALTER TABLE `friendships` DISABLE KEYS */;
+INSERT INTO `friendships` VALUES (1,'1','4','2018-03-14 11:17:33','2018-03-14 11:17:33'),(2,'1','5','2018-03-14 11:19:12','2018-03-14 11:19:12'),(3,'1','6','2018-03-14 11:19:15','2018-03-14 11:19:15'),(4,'4','6','2018-03-14 11:19:20','2018-03-14 11:19:20'),(5,'4','5','2018-03-14 11:19:22','2018-03-14 11:19:22'),(6,'5','4','2018-03-14 11:19:26','2018-03-14 11:19:26'),(7,'5','1','2018-03-14 11:19:28','2018-03-14 11:19:28'),(8,'6','1','2018-03-14 11:19:31','2018-03-14 11:19:31'),(9,'6','4','2018-03-14 11:19:40','2018-03-14 11:19:40'),(10,'6','5','2018-03-14 11:19:43','2018-03-14 11:19:43');
+/*!40000 ALTER TABLE `friendships` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-14 12:11:58
